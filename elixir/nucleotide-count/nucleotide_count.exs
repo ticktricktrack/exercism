361c1c5cbe4fda4f04e6c2be14abf3099ad132b2
Rainer Kuhn <ticktricktrack@gmail.com>
2016-11-30 23:06:31 +0000

    use shorthand

--- elixir/nucleotide-count/nucleotide_count.exs
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -2,7 +2,7 @@ defmodule NucleotideCount do
   @nucleotides [?A, ?C, ?G, ?T]
 
   def count(dna, base) do
-    Enum.count(dna, fn x -> x == base end)
+    Enum.count(dna, &(&1 == base))
   end
 
   def histogram(dna) do
