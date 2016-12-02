da4338a990c8c99debf7e76fd6f3b84073ebad1f
Rainer Kuhn <ticktricktrack@gmail.com>
2016-12-02 01:34:41 +0000

    codestyle

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -5,13 +5,13 @@ defmodule SpaceAge do
   @earth_year 31557600
 
   @planets %{
-    :mercury => 0.2408467,
-    :venus => 0.61519726,
-    :mars => 1.8808158,
-    :jupiter => 11.862615,
-    :saturn => 29.447498,
-    :uranus => 84.016846,
-    :neptune => 164.79132,
+    mercury: 0.2408467,
+    venus:   0.61519726,
+    mars:    1.8808158,
+    jupiter: 11.862615,
+    saturn:  29.447498,
+    uranus:  84.016846,
+    neptune: 164.79132,
   }
 
   def age_on(:earth, seconds) do
