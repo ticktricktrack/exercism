2da518684cfae5129a7ffd1708fc59efbd4b016c
Rainer Kuhn <ticktricktrack@gmail.com>
2017-01-27 09:41:58 +0000

    refactor

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,11 +3,6 @@ class Hamming
     raise ArgumentError if strand_a.length != strand_b.length
     strand_a = strand_a.split('')
     strand_b = strand_b.split('')
-
-    difference = 0
-    strand_a.each_with_index do |base, index|
-      difference += 1 if base != strand_b[index]
-    end
-    difference
+    strand_a.zip(strand_b).count { |a, b| a != b }
   end
 end
