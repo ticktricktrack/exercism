0e4c91f84f55851dab8bf7726ed6a25231224556
Rainer Kuhn <ticktricktrack@gmail.com>
2017-02-01 08:37:49 +0000

    string concat solution

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,10 @@
+class Raindrops
+  def self.convert(quantity)
+    result = ''
+    result << 'Pling' if (quantity % 3).zero?
+    result << 'Plang' if (quantity % 5).zero?
+    result << 'Plong' if (quantity % 7).zero?
+    return quantity.to_s if result == ''
+    result
+  end
+end
