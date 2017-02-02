00a9e4b7ddf0669e5f3d14ae090711ad2a4521f7
Rainer Kuhn <ticktricktrack@gmail.com>
2017-02-02 06:15:46 +0000

    shorten

--- elixir/accumulate/accumulate.exs
+++ elixir/accumulate/accumulate.exs
@@ -1,13 +1,5 @@
 defmodule Accumulate do
-  def accumulate([], _) do
-    []
-  end
-
-  def accumulate([head | []], fun) do
-    [fun.(head)]
-  end
-  
-  @spec accumulate(list, (any -> any)) :: list
+  def accumulate([], _), do: []
   def accumulate([head | tail], fun) do
     [fun.(head) | accumulate(tail, fun)]
   end
